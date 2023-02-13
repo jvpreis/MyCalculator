7d03bc6f53006e2b1c3990d6ec6654dccae4a58a
jose reis <jvpreis@gmail.com>
2023-02-13 11:31:59 +0000

    alteração STUB

--- MyCalculator/src/main/java/MyCalculator/MyCalculator/MyCalculator.java
+++ MyCalculator/src/main/java/MyCalculator/MyCalculator/MyCalculator.java
@@ -21,7 +21,7 @@ public class MyCalculator extends Frame {
 	double number, memValue;
 	char op;
 
-	String digitButtonText[] = {"7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "+/-", "."};
+	String digitButtonText[] = {"7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "+/-", "." };
 	String operatorButtonText[] = { "/", "sqrt", "*", "%", "-", "1/X", "+", "=" };
 	String memoryButtonText[] = { "MC", "MR", "MS", "M+" };
 	String specialButtonText[] = { "Backspc", "C", "CE" };
